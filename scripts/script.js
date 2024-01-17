c888a2d1cf4cb1904c6daf3ca1c6ce967ea7418d
Joaquin Sanchez <joaquinsanchezmiravalles@alumnos.tuniversformacion.es>
2024-01-17 08:58:24 +0100

    Añadimos al script.js ya las primeras concataciones entre el html y el js

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,7 @@
 let formulario = document.querySelector("#formulario");
 let nombre = document.querySelector("#nombre");
 let radio = document.querySelector("#radio");
+let radio2 = document.querySelector("#radio2");
+let mensaje = document.querySelector("#mensaje");
+let enviar = document.querySelector("#enviar");
+let error = document.querySelector("#error");
